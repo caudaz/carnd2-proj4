f67eeb456bd17701f603c98570bedd51307507fa
root <root@fx8350.localdomain>
2017-08-16 14:37:33 -0500

    working with values 0.15 0.001 2.0

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,22 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	PID::Kp = Kp;
+	PID::Ki = Ki;
+	PID::Kd = Kd;
+	
+	p_error = 0.0;
+    i_error = 0.0;
+    d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+    d_error = cte - p_error; // assumes dt=1
+	i_error = i_error + cte; // assumes dt=1
+	p_error = cte;
 }
 
 double PID::TotalError() {
+    return p_error * Kp + d_error * Kd + i_error * Ki;
 }
 
