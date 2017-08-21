0c1d187187601c559669f66c142fc6c9a3c75363
root <root@fx8350.localdomain>
2017-08-21 10:41:32 -0500

    FINAL 0.15, 0.001, 1.75

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,7 @@ int main()
   // TODO: Initialize the pid variable.
   
   //if (iter == 0 && step ==0 ){pid.Init(0.00, 0.001, 0.00);}
-  pid.Init(0.5, 0.001, 4.00); // USE this line if not GLOBAL optimization
+  pid.Init(0.15, 0.001, 1.75); // USE this line if not GLOBAL optimization
   
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
